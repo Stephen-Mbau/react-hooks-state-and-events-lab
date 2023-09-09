b761eab50aae50116adcc78be3034739c439bf42
Stephen Mbau <mbaustephen@gmail.com>
2023-09-09 09:19:17 +0300

    done

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
-  return (
-    <li className="">
+  const [isInCart, setIsInCart] = useState(false);
+  const toggleCartStatus = () => {
+    setIsInCart(!isInCart);
+  };
+  const buttonText = isInCart ? "Remove From Cart" : "Add to Cart";
+  const liClassName = isInCart ? "in-cart" : "";
+return (
+    <li className={liClassName}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={toggleCartStatus}>
+        {buttonText}
+      </button>
     </li>
   );
 }
 
 export default Item;
+
+
+
+
+
+
